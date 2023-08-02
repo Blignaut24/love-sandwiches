1dd700338342cb64b34d724fe78b067e8c6210a4
Johann Jurgens Blignaut <112351511+Blignaut24@users.noreply.github.com>
2023-08-03 00:22:07 +0200

    wip: Calculating our Stock Averages

--- run.py
+++ run.py
@@ -63,11 +63,11 @@ def validate_data(values):
 
 
 def update_worksheet(data, worksheet):
-    """"
-    Receives a list of integers to be inserted into a worksheet
-    update the relevant worksheet with the data provided
     """
-    print(f"updating {worksheet} worksheet...\n")
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet updated successfully\n")
@@ -94,24 +94,19 @@ def calculate_surplus_data(sales_row):
 
 
 def get_last_5_entries_sales():
-    """"
+    """
     Collects columns of data from sales worksheet, collecting
     the last 5 entries for each sandwich and returns the data
     as a list of lists.
     """
     sales = SHEET.worksheet("sales")
     
-    """"
-    FIXME: The code below is not working as expected, 
-    Love Sanwiches 17 Getting our Sales Data
-    YouTube: 06:27
-    """
     columns = []
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
 
-return columns
+    return columns
 
 def main():
     """
@@ -125,6 +120,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-#main()
+main()
 
-sales_columns = get_last_5_entries_sales()
+
